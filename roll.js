8ec4c7b22801335e4421f06fd5009a401ef45a7c
RainLin05 <rainl5@nycstudents.net>
2024-01-09 15:16:39 -0500

    draft

---
+++ roll.js
@@ -0,0 +1,15 @@
+ function dices(num){
+    const number = num
+    const maxFreq = 18
+    const minFreq = 3
+    if(num === "1") {
+        maxFreq = 6
+        minFreq = 1
+    } else if(num === "2") {
+        maxFreq = 12
+        minFreq = 2
+    }
+    for(let i = 0; i < (maxFreq - minFreq); i++) {
+        let arr = []
+    }
+ }
