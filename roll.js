b7f76576f625ed113f85c0cad0b3c8ea973446c1
RainLin05 <rainl5@nycstudents.net>
2024-01-11 15:06:20 -0500

    yes

--- roll.js
+++ roll.js
@@ -1,15 +1,52 @@
- function dices(num){
+let maxFreq = 0
+let minFreq = 0
+var rolls = 0
+let mode = 0
+let median = 0
+let mean = 0
+//console.log(numOfRolls)
+
+
+function dices(num){
     const number = num
-    const maxFreq = 18
-    const minFreq = 3
-    if(num === "1") {
-        maxFreq = 6
-        minFreq = 1
-    } else if(num === "2") {
-        maxFreq = 12
-        minFreq = 2
+    
+    if(number === 1) {
+        maxFreq = 6;
+        minFreq = 1;
+    } else if(number === 2) {
+        maxFreq = 12;
+        minFreq = 2;
+    } else{
+        maxFreq = 18;
+        minFreq = 3;
     }
-    for(let i = 0; i < (maxFreq - minFreq); i++) {
-        let arr = []
+    console.log(minFreq)
+    console.log(maxFreq)
+}
+
+
+function roll(rolls) {
+    let arr = []
+    for(let i = 0; i < rolls; i++) {
+        arr.push(Random())            
     }
- }
+    arr.sort()
+
+    const sum = arr.reduce((acc, curr) => acc + curr, 0); 
+    const average = sum / arr.length; 
+    mean = average; 
+
+    let middle = Math.floor(rolls / 2)
+    median = arr[Math.floor(arr.length/middle)]
+    console.log(median)
+    console.log(mean)
+
+    //document.getElementById().innerText = mode
+    //document.getElementById().innerText = median
+    //document.getElementById().innerText = mean
+}
+
+
+function Random() {
+    return Math.floor(Math.random() * 6 + 1);
+}
